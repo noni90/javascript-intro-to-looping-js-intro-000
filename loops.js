649e3e40106542fdf97256bb01ebf70537a59b0c
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-21 22:03:48 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,3 +19,19 @@ function whileLoop(n)
   }
   return 'done';
 }
+
+function doWhileLoop(num)
+{
+  let i=0;
+  function inc(i)
+  {
+    i=i+1;
+    return i ;
+  }
+  do {
+console.log("I run once regardless.");}
+while (inc(i)<num) 
+  
+}
+  }
+}
